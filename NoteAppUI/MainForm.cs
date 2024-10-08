09f0d72c504914723091481b79e6f08a96e4138f
Valerii Tarasov <145185763+LPAlert@users.noreply.github.com>
2024-10-09 01:47:04 +0300

    Созданы типы данных NoteCategory, Project, PorjectManager

--- NoteAppUI/MainForm.cs
+++ NoteAppUI/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NoteApp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,42 @@ namespace NoteAppUI
         public MainForm()
         {
             InitializeComponent();
+            this.Text = "Главное окно программы";
+            this.Size = new Size(400, 250);
+            var project = new Project();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //Contact firstContact = new Contact();
+            //firstContact.Name = "Alex";
+            //firstContact.Birth = DateTime.Today;
+            //firstContact.Sername = "Ivanov";
+            //firstContact.Email = "some@email.ru";
+            //firstContact.IdVk = "123456789";
+
+            //ProjectManager.SaveToFile( ProjectManager.DefaultPath;
+            //JsonSerializer serializer = new JsonSerializer();
+
+            ////Открываем поток для записи в файл с указанием пути
+            //using (StreamWriter sw = new StreamWriter(@"C:\Users\Денис\json.txt"))
+            //using (JsonWriter writer = new JsonTextWriter(sw))
+            //{
+            //    //Вызываем сериализацию и передаем объект, который хотим сериализовать
+            //    serializer.Serialize(writer, firstContact);
+            //}
+
+        }
+
+        private void MainForm_Load_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
